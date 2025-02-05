2c63ec5e28641c238ff17fdf1095ca4e775397d4
karthiksuk1 <18976081-karthiksuk1@users.noreply.replit.com>
2025-02-05 16:18:55 +0000

    Restored to '70a4b042e1b639482049ab0a800f0c38162906a5'
    Replit-Restored-To:70a4b042e1b639482049ab0a800f0c38162906a5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "./lib/themes";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
@@ -16,13 +15,13 @@ function Router() {
   );
 }
 
-export default function App() {
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <Router />
-        <Toaster />
-      </ThemeProvider>
+      <Router />
+      <Toaster />
     </QueryClientProvider>
   );
 }
+
+export default App;
